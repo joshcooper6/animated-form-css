8ed0aafef7d4de19417cbaa3baa695c0e724ee6a
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2022-10-09 11:15:44 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,26 @@
+export default function Form() {
+    return(<>
+        <div className="box">
+            <div className='form'>
+                <div className="content">
+                    <h2>Sign In</h2>
+                    <div className='inputBox'>
+                        <input type={'text'} required={true} />
+                        <span>Username</span>
+                        <i></i>
+                    </div>
+                    <div className='inputBox'>
+                        <input type={'password'} required={true} />
+                        <span>Password</span>
+                        <i></i>
+                    </div>
+                    <div className="links">
+                        <a href={'#'} children={'Forgot Password'} />
+                        <a href={'#'} children={'Sign Up'} />
+                    </div>
+                    <button className="submit" onClick={(e) => {alert('working')}} children={'Login'} />
+                </div>
+            </div>
+        </div>
+    </>)
+}
